6d534adb213a497617c7b062beaec94abc135954
genuineunlocker <genuineunlockerinfo@gmail.com>
2025-07-12 00:57:47 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,15 @@ const orderRoutes = require('./routes/orderRoutes');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// ✅ Working CORS setup
+const corsOptions = {
+  origin: 'https://genuineunlocker.net',
+  credentials: true,
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
 // Middleware
-app.use(cors({
-  origin: ['https://genuineunlocker.net']// Allow only your frontend's URL
-}));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
