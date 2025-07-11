a8ac2765241e8d09901de204a04c361ca7565ec4
genuineunlocker <genuineunlockerinfo@gmail.com>
2025-07-12 00:45:45 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,14 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['https://genuineunlocker.net']// Allow only your frontend's URL
+  origin: 'https://genuineunlocker.net',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
 }));
+
+app.options('*', cors()); // handle preflight
+
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
