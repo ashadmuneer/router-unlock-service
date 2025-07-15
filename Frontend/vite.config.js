4ddc8c38e32a294ec69e39082829dee6ae78abb1
ashadmuneer <ashadmuneer786@gmail.com>
2025-07-16 01:51:37 +0530

    update

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,16 +1,21 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/',
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'import.meta.env.VITE_API_URL',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  // Load environment variables
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    base: '/',
+    server: {
+      port: 5173,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+        },
       },
     },
-  },
+  };
 });
